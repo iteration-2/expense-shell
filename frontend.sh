9c726d33c8c5f08351b09f412e147bc0205a1861
pappik <paparao.prema75@gmail.com>
2024-10-19 19:38:12 -0400

    pwd

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,11 @@
 dnf install nginx -y
-ls
+pwd
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-ls
+pwd
 #vim /etc/nginx/default.d/expense.conf
 systemctl restart nginx
