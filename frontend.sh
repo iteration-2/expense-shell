d5450e976ba0744bbac02dff11cb19af0c734fba
pappik <paparao.prema75@gmail.com>
2024-10-19 19:37:21 -0400

    testing location to write script for copy

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,11 @@
 dnf install nginx -y
+ls
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
+ls
 #vim /etc/nginx/default.d/expense.conf
 systemctl restart nginx
