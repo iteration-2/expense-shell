980a7e5682283ec882ab6e4d571d655c07a9cd14
pappik <paparao.prema75@gmail.com>
2024-10-19 19:40:45 -0400

    copy config file

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,11 @@
 dnf install nginx -y
-pwd
+
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-pwd
-#vim /etc/nginx/default.d/expense.conf
+
+cp /home/ec2-user/expense-shell/files/expense.conf /etc/nginx/default.d/expense.conf
 systemctl restart nginx
