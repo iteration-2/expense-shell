310cf8d32f7512baa5e44a21562de42c5a4bac4d
pappik <paparao.prema75@gmail.com>
2024-10-19 20:04:18 -0400

    missed mysql repo content

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,4 @@
-dnf module disable mysql -y
-cp /home/ec2-user/expense-shell/files/mysql.repo /etc/yum.repos.d/mysql.repo
-dnf install mysql-community-server -y
+dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
 mysql_secure_installation --set-root-pass ExpenseApp@1
